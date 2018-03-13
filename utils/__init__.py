a240b8c0cfdf353da050ed5668cfcb2ed6f66d52
jmigual <j.marce.igual@gmail.com>
2018-03-13 12:29:49 -0400

    Add new utils module with the ScanNormalizer

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .scan_normalizer import ScanNormalizer
---
+++ utils/scan_normalizer.py
@@ -0,0 +1,12 @@
+
+
+class ScanNormalizer:
+
+    def __init__(self, dirs, output_dir):
+        self.dirs = dirs
+        self.output_dir = output_dir
+
+    def process_images(self):
+        pass
+
+
