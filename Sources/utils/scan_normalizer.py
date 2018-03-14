c4082cea9c17db7b6015a24557d7de2f05d19254
jmigual <j.marce.igual@gmail.com>
2018-03-14 16:44:29 -0400

    Finish preprocessing algorithm

--- Sources/utils/scan_normalizer.py
+++ Sources/utils/scan_normalizer.py
@@ -2,8 +2,8 @@ import os
 import math
 import pydicom
 import numpy as np
-import json
-import scipy.misc as scm
+import skimage.transform as skt
+import shutil
 
 
 class BoundingBox:
@@ -20,69 +20,97 @@ class BoundingBox:
         self.x_max = self.y_max = self.z_max = -math.inf
 
 
+class PseudoDir:
+    def __init__(self, name, path, is_dir):
+        self.name = name
+        self.path = path
+        self._is_dir = is_dir
+
+    def is_dir(self):
+        return self._is_dir
+
+
 class ScanNormalizer:
     MIN_BOUND = -1000.0
     MAX_BOUND = 400.0
 
-    def __init__(self, dirs, output_dir):
-        self.dirs = dirs
+    X_SIZE = 64
+    Y_SIZE = 64
+    Z_SIZE = 64
+
+    def __init__(self, dirs, output_dir, overwrite=False):
+        self.dirs = []
         self.output_dir = output_dir
+        self.count = 0
+        self.overwrite = overwrite
+
+        for d in dirs:
+            self.dirs.append(PseudoDir(d.name, d.path, d.is_dir()))
 
     def process_images(self):
-        list(map(self.process_single, self.dirs))
+        for i, image in enumerate(self.dirs):
+            self.process_single(image, i + 1)
 
-    def process_single(self, image_dir: os.DirEntry):
-        # We need to get the two different dirs
-        subs = os.scandir(image_dir.path)
+    def process_single(self, image_dir: os.DirEntry, count):
 
-        main_dir = None
-        mask_dir = None
-        for sub_dir in subs:
-            if sub_dir.name == image_dir.name:
-                main_dir = sub_dir
-            elif sub_dir.name == image_dir.name + "-MASS":
-                mask_dir = sub_dir
+        save_dir = os.path.join(self.output_dir, image_dir.name)
+        temp_dir = os.path.join(self.output_dir, image_dir.name + "_temp")
 
-        if main_dir is None or mask_dir is None:
+        # Check if the directory exists to avoid overwriting it
+        if os.path.exists(save_dir) and not self.overwrite:
+            return
+
+        if not all(x in os.listdir(image_dir.path) for x in [image_dir.name, image_dir.name + "-MASS"]):
             raise FileNotFoundError("Dir {} does not have the necessary files".format(image_dir.name))
 
-        print("Dataset {}".format(main_dir.name))
+        print("Processing dataset {}, {} of {}".format(image_dir.name, count, len(self.dirs)))
 
-        main_files = os.scandir(main_dir)
-        mask_files = os.scandir(mask_dir)
+        if os.path.exists(temp_dir):
+            shutil.rmtree(temp_dir)
+        os.makedirs(temp_dir)
 
-        total_mask = []
-        total_main = []
+        main_stack, mask_stack = self.compact_files(image_dir)
 
-        new_dir_name = os.path.join(self.output_dir, image_dir.name)
-        os.makedirs(new_dir_name)
+        # Get sliced image
+        x_min, x_max, y_min, y_max, z_min, z_max = self.get_bounding_box(mask_stack)
+        sliced = main_stack[x_min:x_max, y_min:y_max, z_min:z_max]
 
-        for main, mask in zip(main_files, mask_files):
-            # Safe check, make sure that both files have the same name
-            if main.name != mask.name:
-                raise FileNotFoundError("The two files do not have the same name")
+        # Normalize the sliced part
+        sliced = sliced.clip(self.MIN_BOUND, self.MAX_BOUND)
+        sliced_norm = (sliced - self.MIN_BOUND)/(self.MAX_BOUND - self.MIN_BOUND)
 
-            main_dcm = pydicom.dcmread(main.path)
-            mask_dcm = pydicom.dcmread(mask.path)
+        # Apply mask
+        sliced_norm *= mask_stack[x_min:x_max, y_min:y_max, z_min:z_max]
 
-            main_x = main_dcm.pixel_array
-            mask_x = mask_dcm.pixel_array
-            mask_x[mask_x > 1] = 1
-            total_mask.append(mask_x)
+        print("Volume: {}".format(sliced_norm.shape))
 
-            # Normalize the image
-            main_x = main_x.clip(self.MIN_BOUND, self.MAX_BOUND)
-            main_norm = (main_x - self.MIN_BOUND)/(self.MAX_BOUND - self.MIN_BOUND)
+        # Resize the normalized data
+        sliced_norm = skt.resize(sliced_norm, (self.X_SIZE, self.Y_SIZE, self.Z_SIZE), mode='symmetric')
+        np.save(os.path.join(temp_dir, "normalized_slice.npy"), sliced_norm)
 
-            main_norm *= mask_x
-            total_main.append(main_norm)
+        # Rename after finishing to be able to stop in the middle
+        os.rename(temp_dir, save_dir)
+
+        # For debugging purposes it saves all the images
+        # for i in range(self.Z_SIZE):
+        #     print("Saving {} of {}".format(i + 1, self.Z_SIZE))
+        #     scipy.misc.imsave(os.path.join(save_dir, "deb_{}.png".format(i)), sliced_norm[:, :, i]*255)
+        #
+
+    @staticmethod
+    def compact_files(image_dir):
+        main_path = os.path.join(image_dir.path, image_dir.name)
+        mask_path = main_path + "-MASS"
+        total_main = [pydicom.dcmread(x.path).pixel_array for x in os.scandir(main_path)]
+        total_mask = [pydicom.dcmread(x.path).pixel_array for x in os.scandir(mask_path)]
 
         main_stack = np.stack(total_main, axis=2)
         mask_stack = np.stack(total_mask, axis=2)
+        mask_stack[mask_stack > 1] = 1
+        return main_stack, mask_stack
 
-        # TODO: Use scipy zoom to reshape image
-
-        # Get bounding box
+    @staticmethod
+    def get_bounding_box(mask_stack):
         x = np.any(mask_stack, axis=(1, 2))
         y = np.any(mask_stack, axis=(0, 2))
         z = np.any(mask_stack, axis=(0, 1))
@@ -90,19 +118,4 @@ class ScanNormalizer:
         x_min, x_max = np.where(x)[0][[0, -1]]
         y_min, y_max = np.where(y)[0][[0, -1]]
         z_min, z_max = np.where(z)[0][[0, -1]]
-
-        print(type(x_min))
-
-        cut = main_stack[x_min:x_max, y_min:y_max, z_min:z_max]
-        print(cut.shape)
-        cut2 = scm.imresize(cut, (300, 300, 300))
-        print(cut2.shape)
-
-        np.save(os.path.join(new_dir_name, "main_masked.npy"), main_stack)
-        np.save(os.path.join(new_dir_name, "mask.npy"), mask_stack)
-
-
-
-
-
-
+        return x_min, x_max, y_min, y_max, z_min, z_max
