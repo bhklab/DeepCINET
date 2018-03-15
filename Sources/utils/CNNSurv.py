c18c109c1a04a2fb1760beed0e844a5d6829b531
jmigual <j.marce.igual@gmail.com>
2018-03-15 13:31:01 -0400

    Start creating computation graph

--- Sources/utils/CNNSurv.py
+++ Sources/utils/CNNSurv.py
@@ -13,6 +13,34 @@ class CNNSurv:
 
         # The y_data contains the Time and the Event -> 2 dimensions
         self.y_data = tf.placeholder(tf.float32, shape=(None, 2))
+        self._create_computation_graph()
+
+    def _create_computation_graph(self):
+        # In this case the graph follows two different paths, the image path and the data path until they get merged
+        # in the middle
+
+        image_pass = tf.layers.conv3d(
+            self.x_image,
+            filters=64,
+            kernel_size=(6,)*3,
+            activation=tf.nn.relu
+        )
+
+        data_pass = tf.layers.dense(
+            self.x_data,
+            units=64,
+            activation=tf.nn.relu
+        )
+
+        # Spatial filling, data_pass has shape [batch, units], we need to convert it to [batch, height, width, units]
+        image_shape = tf.shape(image_pass)
+        image_shape = image_shape[1:(len(image_shape) - 1)]  # support for 2D and 3D
+
+        data_pass = tf.expand_dims(data_pass, -1)
+        data_pass = tf.expand_dims(data_pass, -1)
+        data_pass = tf.expand_dims(data_pass, -1)
+        data_pass = tf.tile(data_pass, [1, 1, *image_shape])
+
 
 
 
