a4499dfd38a26cb7001a74349bd694d3e0bef7ef
jmigual <j.marce.igual@gmail.com>
2018-03-15 16:16:28 -0400

    Add gather experiment

--- Sources/experimental.py
+++ Sources/experimental.py
@@ -9,17 +9,25 @@ if __name__ == '__main__':
     print(im_shape)
 
     a = tf.Variable([[1, 2, 3], [4, 5, 6]], tf.float32)
+    take = tf.Variable([0])
     b = tf.expand_dims(tf.to_float(a), axis=-1)
     b = tf.expand_dims(b, axis=-1)
     c = tf.tile(b, [1, 1, *im_shape[1:-1]])
     c = tf.transpose(c, [0, 2, 3, 1])
+    zero = tf.Variable(0., tf.float32)
     print(c.dtype)
 
     d = tf.concat([c, im], axis=-1)
+    e = tf.where(d > 5, d, tf.zeros(d.shape))
+
+    init = tf.global_variables_initializer()
+
+    # Sum
+    for i in range(2):
+        zero = zero + tf.reduce_sum(d[i])
 
     with tf.Session() as sess:
-        sess.run([a.initializer])
+        sess.run(init)
 
-        # print(sess.run([a, im]))
-        print(sess.run([tf.shape(d), d]))
+        print(sess.run([zero, tf.reduce_sum(e), tf.gather(e, take)]))
 
