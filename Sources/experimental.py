9089912251736611ad209f43f255abad260530db
jmigual <j.marce.igual@gmail.com>
2018-03-15 15:27:49 -0400

    Add more experiments to test the concat operation

--- Sources/experimental.py
+++ Sources/experimental.py
@@ -4,22 +4,22 @@ import numpy as np
 
 if __name__ == '__main__':
     # Just some text example code
-    im = tf.placeholder(tf.float32, shape=[None, 2, 2, 5])
+    im = tf.zeros([2, 3, 3, 1], tf.float32)
     im_shape = im.get_shape().as_list()
     print(im_shape)
 
     a = tf.Variable([[1, 2, 3], [4, 5, 6]], tf.float32)
-    b = tf.expand_dims(a, axis=-1)
+    b = tf.expand_dims(tf.to_float(a), axis=-1)
     b = tf.expand_dims(b, axis=-1)
     c = tf.tile(b, [1, 1, *im_shape[1:-1]])
     c = tf.transpose(c, [0, 2, 3, 1])
+    print(c.dtype)
+
+    d = tf.concat([c, im], axis=-1)
 
     with tf.Session() as sess:
         sess.run([a.initializer])
 
-        print(sess.run([a]))
-
-        print(sess.run([tf.shape(b), tf.shape(c)]))
-
-        print(sess.run([b, c, im], feed_dict={im: np.zeros((5, 2, 2, 5))}))
+        # print(sess.run([a, im]))
+        print(sess.run([tf.shape(d), d]))
 
