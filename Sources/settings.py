9b5d05d8de343ac39e0983018fedd548bc566c5d
jmigual <j.marce.igual@gmail.com>
2018-04-10 11:22:07 -0400

    Remove .env from the repository so everyone can create their own .env file

--- Sources/settings.py
+++ Sources/settings.py
@@ -5,9 +5,7 @@ APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
 
 dotenv.load_dotenv(os.path.join(APP_ROOT, ".env"), verbose=True)
 
-os.environ['DATA'] = os.path.join(APP_ROOT, os.getenv('DATA'))
-
 # Append APP_ROOT to DATA* environment variables
 for k, v in list(os.environ.items()):
     if str(k).startswith('DATA'):
-        os.environ[k] = os.path.abspath(os.path.join(APP_ROOT, os.getenv(k)))
+        os.environ[k] = os.path.abspath(os.getenv(k))
