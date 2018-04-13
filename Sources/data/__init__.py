e3f102088f47fb0af0db76a53953282fa9774e35
jmigual <j.marce.igual@gmail.com>
2018-04-13 13:29:27 -0400

    Add proper documentation for data module

--- Sources/data/__init__.py
+++ Sources/data/__init__.py
@@ -1,5 +1,7 @@
 """
-This is a test
+The data package contains different classes related in how to read and pre-process data.
+There are also some classes useful for getting all the different pairs in the dataset and
+get it in form of different batches
 
 """
 from data.image_data import RawData, PreProcessedData
