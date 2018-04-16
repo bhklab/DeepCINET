6e416b05791e474c1bbe3cff05c78fc441f41b04
jmigual <j.marce.igual@gmail.com>
2018-04-16 16:40:10 -0400

    Start training

--- Sources/train.py
+++ Sources/train.py
@@ -1,15 +1,39 @@
 import argparse
 
-import data.pair_data
-import settings
+import tensorflow as tf
+
 import data
+import models
+import settings
 
 
 def main(args):
     dataset = data.pair_data.SplitPairs()
     dataset.print_pairs()
-    batch = data.pair_data.BatchData()
-    batch.batches(list(dataset.train_pairs()))
+    batch_data = data.pair_data.BatchData()
+
+    siamese_model = models.Siamese()
+
+    optimizer = tf.train.AdamOptimizer()
+    loss_tensor = siamese_model.loss()
+    minimize_step = optimizer.minimize(loss_tensor)
+    train_pairs = list(dataset.train_pairs())[:1000]
+    print(f"We have {len(train_pairs)} pairs")
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for i, batch in enumerate(batch_data.batches(train_pairs)):
+            # print(batch.images)
+
+            _, loss = sess.run([minimize_step, loss_tensor], feed_dict={
+                siamese_model.x: batch.images,
+                siamese_model.pairs_a: batch.pairs_a,
+                siamese_model.pairs_b: batch.pairs_b,
+                siamese_model.y: batch.labels
+            })
+
+            print(f"Batch: {i}, loss: {loss}")
 
 
 if __name__ == '__main__':
