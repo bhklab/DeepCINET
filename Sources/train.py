1ce0eb9c7ce9104ac4e10225ec14244af748e43b
jmigual <j.marce.igual@gmail.com>
2018-04-20 16:31:35 -0400

    Add operation to evaluate performance between epochs

--- Sources/train.py
+++ Sources/train.py
@@ -19,36 +19,67 @@ def main(args):
 
     siamese_model = models.Siamese()
 
-    optimizer = tf.train.AdamOptimizer()
-    loss_tensor = siamese_model.loss()
-    minimize_step = optimizer.minimize(loss_tensor)
-    train_pairs = list(dataset.train_pairs())
-    total_pairs = len(train_pairs)
-    logger.info(f"We have {total_pairs} pairs")
+    tensor_loss = siamese_model.loss()
+    tensor_c_index = siamese_model.c_index()
+    tensor_true_sum = siamese_model.good_predictions_count()
 
-    c_index = siamese_model.c_index()
+    optimizer = tf.train.AdamOptimizer()
+    tensor_minimize = optimizer.minimize(tensor_loss)
+
+    train_pairs = list(dataset.train_pairs())
+    test_pairs = list(dataset.test_pairs())
+
     conf = tf.ConfigProto()
     conf.gpu_options.allow_growth = True
+
+    saver = tf.train.Saver()
 
     with tf.Session(config=conf) as sess:
         sess.run(tf.global_variables_initializer())
 
         logger.info("Starting training")
 
-        for i, batch in enumerate(batch_data.batches(train_pairs, batch_size=7)):
-            # print(batch.images)
+        for i in range(settings.NUM_EPOCHS):
 
-            _, c_index_result, loss = sess.run([minimize_step, c_index, loss_tensor], feed_dict={
-                siamese_model.x: batch.images,
-                siamese_model.pairs_a: batch.pairs_a,
-                siamese_model.pairs_b: batch.pairs_b,
-                siamese_model.y: batch.labels
-            })
+            total_pairs = len(train_pairs)
+            logger.info(f"Epoch: {i + 1}\tWe have {total_pairs} pairs")
 
-            total_pairs -= len(batch.pairs_a)
-            logger.info(f"Batch: {i}, size: {len(batch.pairs_a)}, remaining pairs: {total_pairs}, "
-                        f"c-index: {c_index_result}, loss: {loss}")
-            # break
+            # Train iterations
+            for j, batch in enumerate(batch_data.batches(train_pairs, batch_size=settings.DATA_BATCH_SIZE)):
+                # Execute graph operations
+                _, c_index_result, loss = sess.run([tensor_minimize, tensor_c_index, tensor_loss], feed_dict={
+                    siamese_model.x: batch.images,
+                    siamese_model.pairs_a: batch.pairs_a,
+                    siamese_model.pairs_b: batch.pairs_b,
+                    siamese_model.y: batch.labels
+                })
+
+                saver.save(sess, settings.SESSION_SAVE_PATH)
+
+                total_pairs -= len(batch.pairs_a)
+                logger.info(f"Batch: {j}, size: {len(batch.pairs_a)}, remaining pairs: {total_pairs}, "
+                            f"c-index: {c_index_result}, loss: {loss}")
+
+            # After we iterate over all the data inspect the test error
+            total_pairs = len(test_pairs)
+            correct_count = 0  # To store correct predictions
+
+            # Test iterations
+            for j, batch in enumerate(batch_data.batches(test_pairs, batch_size=settings.DATA_BATCH_SIZE)):
+                # Execute test operations
+                temp_sum, c_index_result = sess.run([tensor_true_sum, tensor_c_index], feed_dict={
+                    siamese_model.x: batch.images,
+                    siamese_model.pairs_a: batch.pairs_a,
+                    siamese_model.pairs_b: batch.pairs_b,
+                    siamese_model.y: batch.labels
+                })
+
+                correct_count += temp_sum
+                total_pairs -= len(batch.pairs_a)
+                logger.info(f"Batch: {j}, size: {len(batch.pairs_a)}, remaining pairs: {total_pairs}, "
+                            f"c-index: {c_index_result}")
+
+            logger.info(f"Final c-index: {correct_count/len(test_pairs)}")
 
 
 if __name__ == '__main__':
