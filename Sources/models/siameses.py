8d02fe9db7411b4d05df0fba9629e67168c3549d
jmigual <j.marce.igual@gmail.com>
2018-04-25 11:46:29 -0400

    Move convolutional and fully connected methods to derived class

--- Sources/models/siameses.py
+++ Sources/models/siameses.py
@@ -1,3 +1,5 @@
+import abc
+
 import tensorflow as tf
 
 import utils
@@ -35,9 +37,9 @@ class BasicSiamese:
 
         :param gpu_level: Amount of GPU to be used with the model
         """
-        self.gpu_level = gpu_level
+        self._gpu_level = gpu_level
 
-        device = '/gpu:0' if self.gpu_level >= 3 else '/cpu:0'
+        device = '/gpu:0' if self._gpu_level >= 3 else '/cpu:0'
         logger.debug(f"Using device: {device} for parameters")
         with tf.device(device):
             # The input size for the images is 64x64x64x1
@@ -47,9 +49,10 @@ class BasicSiamese:
             self.pairs_a = tf.placeholder(tf.int32, [None], name="pairs_a")
             self.pairs_b = tf.placeholder(tf.int32, [None], name="pairs_b")
 
-            self.batch_size = tf.cast(tf.shape(self.y)[0], tf.float32, name="batch_size_cast")
+        self._sister_out = self._sister(self.x_image)
 
-        self.sister_out = self._sister(self.x_image)
+        #: **Attribute**: Probability of :math:`\hat{y} = 1`
+        self.y_prob = self._contrastive_loss(self._sister_out)
 
         self.y_prob = self._contrastive_loss(self.sister_out)
         self.y_estimate = tf.greater_equal(self.y_prob, self.THRESHOLD)
@@ -71,104 +74,28 @@ class BasicSiamese:
 
         return x
 
-    def _conv_layers(self, x: tf.Tensor):
+    @abc.abstractmethod
+    def _conv_layers(self, x: tf.Tensor) -> tf.Tensor:
         """
-        Implement the convolutional layers of the network
+        Implement this method to create the tensors for the Convolutional layers of the network
 
-        :param x: Network's input images
+        :param x: Network's input images with shape ``[batch, 64, 64, 64, 1]``
         :return: Filtered image with the convolutions applied
         """
-        # In: [batch, 64, 64, 64, 1]
 
-        device = '/gpu:0' if self.gpu_level >= 2 else '/cpu:0'
-        logger.debug(f"Using device: {device} for first conv layers")
-        with tf.device(device):
-            # Out: [batch, 31, 31, 31, 30]
-            x = tf.layers.conv3d(
-                x,
-                filters=30,
-                kernel_size=3,
-                strides=2,
-                activation=tf.nn.relu,
-                name="conv1"
-            )
-
-            # Out: [batch, 29, 29, 29, 40]
-            x = tf.layers.conv3d(
-                x,
-                filters=40,
-                kernel_size=3,
-                activation=tf.nn.relu,
-                name="conv2"
-            )
-
-        device = '/gpu:0' if self.gpu_level >= 1 else '/cpu:0'
-        logger.debug(f"Using device: {device} for second conv layers")
-        with tf.device(device):
-            # Out: [batch, 27, 27, 27, 40]
-            x = tf.layers.conv3d(
-                x,
-                filters=40,
-                kernel_size=3,
-                activation=tf.nn.relu,
-                name="conv3"
-            )
-
-            # Out: [batch, 25, 25, 25, 50]
-            x = tf.layers.conv3d(
-                x,
-                filters=50,
-                kernel_size=3,
-                activation=tf.nn.relu,
-                name="conv4"
-            )
-        return x
-
-    def _fc_layers(self, x: tf.Tensor):
+    @abc.abstractmethod
+    def _fc_layers(self, x: tf.Tensor) -> tf.Tensor:
         """
-        Implement the fully connected layers of the network
+        Implement this method to create the tensors for the Fully Connected layers of the network
 
         :param x: Image, usually previously filtered with the convolutional layers.
         :return: Tensor with shape ``[batch, 1]``
         """
-        device = '/gpu:0' if self.gpu_level >= 3 else '/cpu:0'
-        logger.debug(f"Using device: {device} for FC layers")
-        with tf.device(device):
-            # Out: [batch, 25*25*25*50]
-            x = tf.layers.flatten(
-                x,
-                name="flat"
-            )
-
-            # Out: [batch, 100]
-            x = tf.layers.dense(
-                x,
-                100,
-                activation=tf.nn.relu,
-                name="fc1"
-            )
-
-            # Out: [batch, 50]
-            x = tf.layers.dense(
-                x,
-                50,
-                activation=tf.nn.relu,
-                name="fc2"
-            )
-
-            # Out: [batch, 1]
-            x = tf.layers.dense(
-                x,
-                1,
-                activation=tf.nn.relu,
-                name="fc3"
-            )
-        return x
 
     def _contrastive_loss(self, sister_out: tf.Tensor):
         r"""
-        Implement the loss to compare the two sister networks. To get the pairs to be compared it uses the ``self.pairs_a``
-        and ``self.pairs_b``. In this case the contrastive loss is as follows:
+        Implement the loss to compare the two sister networks. To get the pairs to be compared it uses the
+        :any:`BasicSiamese.pairs_a` and ``self.pairs_b``. In this case the contrastive loss is as follows:
 
         .. math::
             G_W(\boldsymbol{X_A}) &:= \text{Outputs for inputs A} \\
@@ -180,7 +107,7 @@ class BasicSiamese:
                            be compared.
         :return: Tensor with the contrastive loss, comparing the two sister's output.
         """
-        device = '/gpu:0' if self.gpu_level >= 3 else '/cpu:0'
+        device = '/gpu:0' if self._gpu_level >= 3 else '/cpu:0'
         logger.debug(f"Using device: {device} for contrastive loss")
         with tf.device(device):
             gathered_a = tf.gather(sister_out, self.pairs_a, name="contrastive_gather_a")
@@ -216,12 +143,101 @@ class BasicSiamese:
         return tf.cast(tf.count_nonzero(equals), tf.float32)
 
     def c_index(self):
-        return self.good_predictions_count()/self.batch_size
+        batch_size = tf.cast(tf.shape(self.y)[0], tf.float32, name="batch_size_cast")
+        return self.good_predictions_count()/batch_size
 
 
 class SimpleSiamese(BasicSiamese):
     def __init__(self, gpu_level: int = 0):
         super().__init__(gpu_level)
+
+    def _conv_layers(self, x: tf.Tensor) -> tf.Tensor:
+        """
+        Implementation of abstract method :func:`~BasicSiamese._conv_layers`
+
+        :param x:
+        :return:
+        """
+        # In: [batch, 64, 64, 64, 1]
+
+        device = '/gpu:0' if self._gpu_level >= 2 else '/cpu:0'
+        logger.debug(f"Using device: {device} for first conv layers")
+        with tf.device(device):
+            # Out: [batch, 31, 31, 31, 30]
+            x = tf.layers.conv3d(
+                x,
+                filters=30,
+                kernel_size=3,
+                strides=2,
+                activation=tf.nn.relu,
+                name="conv1"
+            )
+
+            # Out: [batch, 29, 29, 29, 40]
+            x = tf.layers.conv3d(
+                x,
+                filters=40,
+                kernel_size=3,
+                activation=tf.nn.relu,
+                name="conv2"
+            )
+
+        device = '/gpu:0' if self._gpu_level >= 1 else '/cpu:0'
+        logger.debug(f"Using device: {device} for second conv layers")
+        with tf.device(device):
+            # Out: [batch, 27, 27, 27, 40]
+            x = tf.layers.conv3d(
+                x,
+                filters=40,
+                kernel_size=3,
+                activation=tf.nn.relu,
+                name="conv3"
+            )
+
+            # Out: [batch, 25, 25, 25, 50]
+            x = tf.layers.conv3d(
+                x,
+                filters=50,
+                kernel_size=3,
+                activation=tf.nn.relu,
+                name="conv4"
+            )
+        return x
+
+    def _fc_layers(self, x: tf.Tensor) -> tf.Tensor:
+        device = '/gpu:0' if self._gpu_level >= 3 else '/cpu:0'
+        logger.debug(f"Using device: {device} for FC layers")
+        with tf.device(device):
+            # Out: [batch, 25*25*25*50]
+            x = tf.layers.flatten(
+                x,
+                name="flat"
+            )
+
+            # Out: [batch, 100]
+            x = tf.layers.dense(
+                x,
+                100,
+                activation=tf.nn.relu,
+                name="fc1"
+            )
+
+            # Out: [batch, 50]
+            x = tf.layers.dense(
+                x,
+                50,
+                activation=tf.nn.relu,
+                name="fc2"
+            )
+
+            # Out: [batch, 1]
+            x = tf.layers.dense(
+                x,
+                1,
+                activation=tf.nn.relu,
+                name="fc3"
+            )
+        return x
 
 
 class ScalarSiamese:
