ccf1d1b8ad94e043e8d23920f12e7a02132d93ec
jmigual <j.marce.igual@gmail.com>
2018-04-26 11:48:43 -0400

    Added more documentation for data structures

--- Sources/data/data_structures.py
+++ Sources/data/data_structures.py
@@ -14,10 +14,15 @@ class PseudoDir(NamedTuple):
 
 class PairComp(NamedTuple):
     """
-    Class to compare pairs containing two ids and a comparison value saying if
-    T(p1) < T(p2)
+    Tuple class to compare pairs containing two ids and a comparison value saying if
+    :math:`T(p1) < T(p2)`
 
-    :attr comp: True if T(p1) < T(p2)
+    :ivar PairComp.comp: True if :math:`T(p1) < T(p2)`
+    :ivar PairComp.p_a: Key for pair's element A
+    :ivar PairComp.p_b: Key for pair's element B
+    :vartype PairComp.comp: bool
+    :vartype PairComp.p_a: str
+    :vartype PairComp.p_b: str
     """
     p_a: str
     p_b: str
@@ -25,6 +30,31 @@ class PairComp(NamedTuple):
 
 
 class PairBatch(NamedTuple):
+    """
+    Tuple class for a batch of data that can be used to train a machine learning model. It's prepared to load as
+    minimum as possible images since the images can be gathered to create the pairs.
+
+    >>> import numpy as np
+    >>> batch = PairBatch(...)
+    >>> images_a = np.take(batch.images, batch.pairs_a)
+    >>> images_b = np.take(batch.images, batch.pairs_b)
+    >>> len(images_a) == len(images_b) == len(images_b)  # Must-have condition
+    True
+    >>>
+
+    :ivar PairBatch.pairs_a: List of indices where the images can be selected to get the pair's element A
+    :ivar PairBatch.pairs_b: List of indices where the images can be selected to get the pair's element B
+    :ivar PairBatch.labels: Labels for the prediction, the possible values for each label are ``0`` or ``1``
+    :ivar PairBatch.images: All the images for the indices, note that the indices for the pairs must be between
+                            ``0`` and ``len(batch.images) - 1``
+    :ivar PairBatch.ids_map: Dictionary that map each key as :any:`string` with its corresponding index as an
+                             :any:`int`
+    :vartype PairBatch.pairs_a: list[int]
+    :vartype PairBatch.pairs_b: list[int]
+    :vartype PairBatch.labels: list[int]
+    :vartype PairBatch.images: numpy.ndarray
+    :vartype PairBatch.ids_map: dict[str, int]
+    """
     pairs_a: List[int]
     pairs_b: List[int]
     labels: List[int]
