980fa1e53ccbbb4078fff92ef46c80110c2a22a1
farnooshKh <Farnoosh.khodakarami@gmail.com>
2019-04-17 14:28:45 -0400

    add multiple

--- Sources/tensorflow_src/multiple.py
+++ Sources/tensorflow_src/multiple.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 import settings
 
-#import STprediction
+# import STprediction
 from tensorflow_src import train_test_models
 import seaborn as sns
 import random
@@ -14,10 +15,6 @@ import yaml
 import multiprocessing
 
 sns.set()
-
-with open("modelConf.yml", 'r') as cfg_file:
-    cfg = yaml.load(cfg_file)
-
 results = pd.DataFrame()
 mixed_c_index, train_c_index, test_c_index = [], [], []
 running_times = 20
@@ -25,33 +22,33 @@ random_states = list(range(running_times * 2))
 random.seed(1)
 random.shuffle(random_states)
 
-
 for i in range(running_times):
-    counts,predictions = train_test_models.deepCinet(model = 'ClinicalOnlySiamese3',
-                                                     num_epochs=100,
-                                                     batch_size=200,
-                                                     splitting_model=1,
-                                                     learning_rate=0.0003,
-                                                     dropout=.2,
-                                                     threshold = 4,
-                                                     split=i, save_model=True,
-                                                     regularization=0.8,
-                                                     split_seed=random_states[i],
-                                                     initial_seed=None,
-                                                     mrmr_size=0,
-                                                     read_splits=True,
-                                                     full_summary = False,
-                                                     cv_folds=5,
-                                                     split_number=i,
-                                                     data_type="clinical")
-
+    counts, predictions = train_test_models.deepCinet(model='ClinicalVolumeSiamese3',
+                                                      num_epochs=25,
+                                                      batch_size=80,
+                                                      splitting_model=1,
+                                                      learning_rate=0.0003,
+                                                      dropout=.2,
+                                                      threshold=4,
+                                                      split=i, save_model=True,
+                                                      regularization=0.8,
+                                                      split_seed=random_states[i],
+                                                      initial_seed=None,
+                                                      mrmr_size=0,
+                                                      read_splits=True,
+                                                      full_summary=True,
+                                                      cv_folds=5,
+                                                      split_number=i,
+                                                      data_type="clinicalVolume")
 
     print(f"test{[v[1] for v in counts['test']['c_index']]}")
     print(f"test{len([v[1] for v in counts['test']['c_index']])}")
     print(counts)
-    counts['train']['c_index'] = sum([v[1] for v in counts['train']['c_index']]) / float(len(counts['train']['c_index']))
+    counts['train']['c_index'] = sum([v[1] for v in counts['train']['c_index']]) / float(
+        len(counts['train']['c_index']))
     counts['test']['c_index'] = sum([v[1] for v in counts['test']['c_index']]) / float(len(counts['test']['c_index']))
-    counts['mixed']['c_index'] = sum([v[1] for v in counts['mixed']['c_index']]) / float(len(counts['mixed']['c_index']))
+    counts['mixed']['c_index'] = sum([v[1] for v in counts['mixed']['c_index']]) / float(
+        len(counts['mixed']['c_index']))
 
     # Store the mixed c-index values for boxplot
     mixed_c_index.append(counts['mixed']['c_index'])
@@ -63,8 +60,5 @@ for i in range(running_times):
     result['random state'] = random_states[i]
     result['number'] = i
     results = results.append(result)
-    results.to_csv(os.path.join(settings.SESSION_SAVE_PATH,"result.csv"))
-results.to_csv(cfg['mixed_result_path'], index = False)
-
-
-
+    results.to_csv(os.path.join(settings.SESSION_SAVE_PATH, "result.csv"))
+results.to_csv(os.path.join(settings.SESSION_SAVE_PATH, "result.csv"), index=False)
