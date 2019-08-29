36d40c14f88051a93b15165d06a2f94fed9df3d5
farnooshKh <Farnoosh.khodakarami@gmail.com>
2019-08-29 15:33:28 -0400

    add more features to hyper parameter selection

--- Sources/tensorflow_src/gpu_hyperParamSelection.py
+++ Sources/tensorflow_src/gpu_hyperParamSelection.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from test_tube import Experiment, HyperOptArgumentParser
+import config
 import pandas as pd
 import random
-import yaml
+import config
 import os
 import sys
+import shutil
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
@@ -19,7 +21,7 @@ cfg = config.HYPER_PARAM
 running_times = cfg['running_times']
 
 models = cfg['model']
-print(models)
+# print(models)
 # randomly feed random state.
 random_states = list(range(running_times * 2))
 random.seed(1)
@@ -34,24 +36,36 @@ def trainDeepCInet(hparams):
         save_dir=hparams.log_path,
         autosave=True,
     )
+
     results1 = pd.DataFrame()
     exp.argparse(hparams)
     for i in range(running_times):
-        counts, predictions = train_test_models.deepCinet(model=hparams.model,
-                                                          num_epochs=hparams.num_epochs,
-                                                          batch_size=hparams.batch_size,
-                                                          splitting_model=1,
-                                                          split=random_states[i],
-                                                          save_model=True,
-                                                          split_seed=random_states[i],
-                                                          regularization=hparams.regularization,
-                                                          initial_seed=random_states[i],
-                                                          learning_rate=hparams.learningRate,
-                                                          mrmr_size=hparams.mrmr_size,
-                                                          read_splits=True,
-                                                          split_number=i,
-                                                          cv_folds=5
-                                                          )
+        parameters = dict(model=hparams.model,
+                          target_path=hparams.target_path,
+                          feature_path=hparams.feature_path,
+                          input_path=hparams.input_path,
+                          results_path=hparams.result_path,
+                          num_epochs=hparams.num_epochs,
+                          batch_size=hparams.batch_size,
+                          splitting_model=1,
+                          split=random_states[i],
+                          learning_rate=hparams.learningRate,
+                          dropout=.2,
+                          threshold=0.5,
+                          save_model=True,
+                          regularization=hparams.regularization,
+                          split_seed=random_states[i],
+                          initial_seed=random_states[i],
+                          mrmr_size=hparams.mrmr_size,
+                          read_splits=True,
+                          full_summary=False,
+                          cv_folds=1,
+                          split_number=i,
+                          train_distance=0.2,
+                          test_distance=0.2,
+                          survival=False
+                          )
+        counts, predictions = train_test_models.deepCinet(**parameters)
 
         counts['train']['c_index'] = sum([v[1] for v in counts['train']['c_index']]) / float(
             len(counts['train']['c_index']))
@@ -68,44 +82,55 @@ def trainDeepCInet(hparams):
         log_content = {'train': counts['train']['c_index'],
                        'test': counts['test']['c_index'],
                        'mixed': counts['mixed']['c_index'],
-                       'models': [hparams.model],
-                       'num_epochs': [hparams.num_epochs],
-                       'batch_size': [hparams.batch_size],
-                       'regularization': [hparams.regularization],
-                       'learning_rate': [hparams.learningRate],
-                       'mrmr_size': [hparams.mrmr_size]
                        }
+        log_content = {**log_content, **parameters}
         exp.log(log_content)
-        result = pd.DataFrame.from_dict(log_content)
+        result = pd.DataFrame(log_content, index=[i])
         result['random state'] = random_states[i]
         result['number'] = i
         results1 = results1.append(result)
         # if (i % 3 == 0):
         # os.path.join(cfg['mixed_result_path'], f"epoch{hparams.num_epochs}",.csv"
         # results1.to_csv(pd.read_csv()))
-    path = os.path.join(cfg['mixed_result_path'], f"epoch{hparams.num_epochs}", f"batch{hparams.batch_size}",
-                        f"regularization{hparams.regularization}", f"learningRate{hparams.learningRate}",
-                        f"mrmr{hparams.mrmr_size}")
+    path = os.path.join(cfg['mixed_result_path'], f"model{hparams.model}_"
+                                                  f"epoch{hparams.num_epochs}_"
+                                                  f"batch{hparams.batch_size}_"
+                                                  f"regularization{hparams.regularization}_"
+                                                  f"learningRate{hparams.learningRate}_"
+                                                  f"mrmr{hparams.mrmr_size}")
     # print(results1)
-    # results1.to_csv(os.path.join(path,"result.csv"), index=False)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    results1.to_csv(os.path.join(path, "result.csv"), index=False)
     exp.save()
 
 
-# set up our argparser and make the model tunable
-# Use either random_search or grid_search for tuning
-parser = HyperOptArgumentParser(strategy='random_search')
-parser.add_argument('--test_tube_exp_name', default='DeepCINET_ScalarOnlySiamese7')
-parser.add_argument('--log_path', default=cfg['log_path'])
+def hyperParamSelection(target_path: str = config.DATA_PATH_TARGET,
+                        feature_path: str = config.DATA_PATH_FEATURE,
+                        input_path: str = config.DATA_PATH_INPUT_TEST_TRAIN,
+                        results_path: str = config.SESSION_SAVE_PATH):
+    # set up our argparser and make the model tunable
+    # Use either random_search or grid_search for tuning
+    parser = HyperOptArgumentParser(strategy='random_search')
+    parser.add_argument('--test_tube_exp_name', default='DeepCINET_ScalarOnlySiamese')
+    parser.add_argument('--log_path', default=cfg['log_path'])
+    parser.add_argument('--target_path', default=target_path)
+    parser.add_argument('--feature_path', default=feature_path)
+    parser.add_argument('--input_path', default=input_path)
+    parser.add_argument('--result_path', default=results_path)
+    parser.opt_list('--model', default='ScalarOnlySiamese', options=cfg['model'], tunable=True)
+    parser.opt_list('--mrmr_size', default=0, options=cfg['mrmr_size'], tunable=True)
+    parser.opt_list('--num_epochs', default=5, options=cfg['num_epochs'], tunable=True)
+    parser.opt_list('--batch_size', default=40, options=cfg['batch_size'], tunable=True)
+    parser.opt_list('--regularization', default=0.5, options=cfg['regularization'], tunable=True)
+    parser.opt_list('--learningRate', default=0.0002, options=cfg['learningRate'], tunable=True)
 
-parser.opt_list('--model', default='ScalarOnlySiamese', options=cfg['model'], tunable=True)
-parser.opt_list('--mrmr_size', default=0, options=cfg['mrmr_size'], tunable=True)
-parser.opt_list('--num_epochs', default=100, options=cfg['num_epochs'], tunable=True)
-parser.opt_list('--batch_size', default=250, options=cfg['batch_size'], tunable=True)
-parser.opt_list('--regularization', default=0.5, options=cfg['regularization'], tunable=True)
-parser.opt_list('--learningRate', default=0.0001, options=cfg['learningRate'], tunable=True)
+    hyperparams = parser.parse_args()
 
-hyperparams = parser.parse_args()
+    # optimize on 4 gpus at the same time
+    # each gpu will get 1 experiment with a set of hyperparams
+    hyperparams.optimize_parallel_gpu(trainDeepCInet, gpu_ids=['1', '0', '3', '2'])
 
-# optimize on 4 gpus at the same time
-# each gpu will get 1 experiment with a set of hyperparams
-hyperparams.optimize_parallel_gpu(trainDeepCInet, gpu_ids=['1', '0', '3', '2'], nb_trials=4, nb_workers=4)
+
+hyperParamSelection()
--- Sources/tensorflow_src/hyperParamSelection.py
+++ Sources/tensorflow_src/hyperParamSelection.py
@@ -5,23 +5,25 @@ import random
 import config
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+import shutil
 
-#import STprediction
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+# import STprediction
 from tensorflow_src import train_test_models
-
 
 mixed_c_index, train_c_index, test_c_index = [], [], []
 
 cfg = config.HYPER_PARAM
 
-#the number of times which should run a model
+# the number of times which should run a model
 running_times = cfg['running_times']
 
-#randomly feed random state.
-random_states = list(range(running_times* 2))
+# randomly feed random state.
+random_states = list(range(running_times * 2))
 random.seed(1)
 random.shuffle(random_states)
+
 
 # main training function (very simple)
 def trainDeepCInet(hparams):
@@ -34,22 +36,32 @@ def trainDeepCInet(hparams):
     results1 = pd.DataFrame()
     exp.argparse(hparams)
     for i in range(running_times):
-
-        counts, predictions = train_test_models.deepCinet(model = hparams.model,
-                                                          num_epochs= hparams.num_epochs,
-                                                          batch_size= hparams.batch_size,
-                                                          splitting_model=1,
-                                                          split=random_states[i],
-                                                          save_model=True,
-                                                          split_seed=random_states[i],
-                                                          regularization=hparams.regularization,
-                                                          initial_seed=random_states[i],
-                                                          learning_rate=hparams.learningRate,
-                                                          mrmr_size = hparams.mrmr_size,
-                                                          read_splits = True,
-                                                          split_number=i,
-                                                          cv_folds=5
-                                                          )
+        parameters = dict(model=hparams.model,
+                          target_path=hparams.target_path,
+                          feature_path=hparams.feature_path,
+                          input_path=hparams.input_path,
+                          results_path=hparams.result_path,
+                          num_epochs=hparams.num_epochs,
+                          batch_size=hparams.batch_size,
+                          splitting_model=1,
+                          split=random_states[i],
+                          learning_rate=hparams.learningRate,
+                          dropout=.2,
+                          threshold=0.5,
+                          save_model=True,
+                          regularization=hparams.regularization,
+                          split_seed=random_states[i],
+                          initial_seed=random_states[i],
+                          mrmr_size=hparams.mrmr_size,
+                          read_splits=True,
+                          full_summary=False,
+                          cv_folds=1,
+                          split_number=i,
+                          train_distance=0.2,
+                          test_distance=0.2,
+                          survival=False
+                          )
+        counts, predictions = train_test_models.deepCinet(**parameters)
 
         counts['train']['c_index'] = sum([v[1] for v in counts['train']['c_index']]) / float(
             len(counts['train']['c_index']))
@@ -66,43 +78,54 @@ def trainDeepCInet(hparams):
         log_content = {'train': counts['train']['c_index'],
                        'test': counts['test']['c_index'],
                        'mixed': counts['mixed']['c_index'],
-                       'models':hparams.model,
-                       'num_epochs': hparams.num_epochs,
-                       'batch_size': hparams.batch_size,
-                       'regularization': hparams.regularization,
-                       'learning_rate': hparams.learningRate,
-                       'mrmr_size': hparams.mrmr_size
                        }
+        log_content = {**log_content, **parameters}
         exp.log(log_content)
-        result = pd.DataFrame.from_dict(log_content)
+        result = pd.DataFrame(log_content, index=[i])
         result['random state'] = random_states[i]
         result['number'] = i
         results1 = results1.append(result)
-        #if (i % 3 == 0):
-            #os.path.join(cfg['mixed_result_path'], f"epoch{hparams.num_epochs}",.csv"
-            #results1.to_csv(pd.read_csv()))
-    #path = os.path.join(cfg['mixed_result_path'], f"epoch{hparams.num_epochs}", f"batch{hparams.batch_size}", f"regularization{hparams.regularization}",f"learningRate{hparams.learningRate}",f"mrmr{hparams.mrmr_size}")
-    #print(results1)
-    #results1.to_csv(os.path.join(path,"result.csv"), index=False)
+        # if (i % 3 == 0):
+        # os.path.join(cfg['mixed_result_path'], f"epoch{hparams.num_epochs}",.csv"
+        # results1.to_csv(pd.read_csv()))
+    path = os.path.join(cfg['mixed_result_path'], f"model{hparams.model}_"
+                                                  f"epoch{hparams.num_epochs}_"
+                                                  f"batch{hparams.batch_size}_"
+                                                  f"regularization{hparams.regularization}_"
+                                                  f"learningRate{hparams.learningRate}_"
+                                                  f"mrmr{hparams.mrmr_size}")
+    # print(results1)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    results1.to_csv(os.path.join(path, "result.csv"), index=False)
     exp.save()
 
 
-# set up our argparser and make the model tunable
-# Use either random_search or grid_search for tuning
-parser = HyperOptArgumentParser(strategy='random_search')
-parser.add_argument('--test_tube_exp_name', default='DeepCINET_ScalarOnlySiamese9')
-parser.add_argument('--log_path', default=cfg['log_path'])
+def hyperParamSelection(target_path: str = config.DATA_PATH_TARGET,
+                        feature_path: str = config.DATA_PATH_FEATURE,
+                        input_path: str = config.DATA_PATH_INPUT_TEST_TRAIN,
+                        results_path: str = config.SESSION_SAVE_PATH):
+    # set up our argparser and make the model tunable
+    # Use either random_search or grid_search for tuning
+    parser = HyperOptArgumentParser(strategy='random_search')
+    parser.add_argument('--test_tube_exp_name', default='DeepCINET_ScalarOnlySiamese7')
+    parser.add_argument('--log_path', default=cfg['log_path'])
+    parser.opt_list('--model', default='ScalarOnlySiamese', options=cfg['model'], tunable=True)
+    parser.opt_list('--mrmr_size', default=0, options=cfg['mrmr_size'], tunable=True)
+    parser.opt_list('--num_epochs', default=5, options=cfg['num_epochs'], tunable=True)
+    parser.opt_list('--batch_size', default=40, options=cfg['batch_size'], tunable=True)
+    parser.opt_list('--regularization', default=0.5, options=cfg['regularization'], tunable=True)
+    parser.opt_list('--learningRate', default=0.0002, options=cfg['learningRate'], tunable=True)
+    parser.opt_list('--target_path', default=target_path)
+    parser.opt_list('--feature_path', default=feature_path)
+    parser.opt_list('--input_path',default=input_path)
+    parser.opt_list('--result_path', default = results_path)
 
-parser.opt_list('--model', default='ClinicalOnlySiamese', options=cfg['model'], tunable=True)
-parser.opt_list('--mrmr_size', default=0, options= cfg['mrmr_size'], tunable=True)
-parser.opt_list('--num_epochs', default=100, options=cfg['num_epochs'], tunable=True)
-parser.opt_list('--batch_size', default=100, options=cfg['batch_size'], tunable=True)
-parser.opt_list('--regularization', default=0.5, options=cfg['regularization'], tunable=True)
-parser.opt_list('--learningRate', default=0.0003, options=cfg['learningRate'], tunable=True)
+    hyperparams = parser.parse_args()
 
-hyperparams = parser.parse_args()
+    # optimize on 4 gpus at the same time
+    # each gpu will get 1 experiment with a set of hyperparams
+    hyperparams.optimize_parallel_cpu(trainDeepCInet, nb_trials=100, nb_workers=2)
 
-
-# optimize on 4 gpus at the same time
-# each gpu will get 1 experiment with a set of hyperparams
-hyperparams.optimize_parallel_cpu(trainDeepCInet, nb_trials=100, nb_workers=2)
+hyperParamSelection()
