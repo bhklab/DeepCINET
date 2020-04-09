Fixed bug causing data leakage, added a LOT of command line options as well as cross validation now the clinical information and radiomic features are expected to be sorted by time

Sources/pytorch_src/data/dataloader.py
@@ -12,7 +12,6 @@ class Dataset(torch.utils.data.Dataset):
         self.image_path = image_path
         self.clinical_path = clinical_path
         self.clinical_csv = pd.read_csv(clinical_path, index_col=0)
-        print(self.clinical_csv.head())
         self.pyRadiomics_path = pyRadiomics_path
         if(self.hparams.use_radiomics):
             self.radiomics_csv = pd.read_csv(pyRadiomics_path)
@@ -59,35 +58,26 @@ class Dataset(torch.utils.data.Dataset):
     def buildpairs(self, idx_list):
         for i in range(len(idx_list)):
             id1 = idx_list[i]
-            patient1 = self.clinical_csv.iloc[i]
+            patient1 = self.clinical_csv.iloc[id1]
             if(int(patient1['event']) == 1):
-                for j in range(i + 1, len(idx_list)):
-                    if((i + j) % 2 == 1):
+                for j in range(i+1, len(idx_list)):
+                    id2 = idx_list[j]
+                    # patient2 = self.clinical_csv.iloc[id2]
+                    # if(id1 != id2):
+                        # if(patient2['time'] > patient1['time']):
+                            # print("Ids: {} - {}".format(id1, id2))
+                            # print("Clinical Features:")
+                            # print(patient1)
+                            # print(self.clinical_csv.iloc[id2])
+                    if((i + j)%2 == 1):
                         self.pairlist.append({
-                            'pA': i,
-                            'pB': j,
-                            'label': 0}
+                            'pA': id1,
+                            'pB': id2,
+                            'label': 1}
                         )
                     else:
                         self.pairlist.append({
-                            'pA': j,
-                            'pB': i,
-                            'label': 1}
+                            'pA': id2,
+                            'pB': id1,
+                            'label': 0}
                         )
-
-    def comp(self, patient1, patient2):
-        label = -1
-        if(patient1['event'] == 1):
-            if(patient1['time'] < patient2['time']):
-                label = 0
-            elif(patient2['event'] == 1):
-                label = 1
-        else:
-            if(patient2['event'] and patient2['time'] < patient1['time']):
-                label = 1
-        return label
-
-    def standardize(self):
-        for feature in self.radiomics_csv.columns:
-            if(feature not in ['id', 'roi', 'exam']):
-                self.radiomics_csv[feature] = (self.radiomics_csv[feature] - self.radiomics_csv[feature].mean())/(self.radiomics_csv[feature].std())

Sources/pytorch_src/models/distance_model.py
@@ -6,7 +6,11 @@ class DistanceLayer(nn.Module):
         super(DistanceLayer, self).__init__()
 
         self.layer1 = nn.Sequential(
-            nn.Linear(32, 8, bias = False),
+            nn.Linear(32, 16, bias = False),
+            nn.Tanh(),
+            nn.Linear(16, 16, bias = False),
+            nn.Tanh(),
+            nn.Linear(16, 8, bias = False),
             nn.Tanh(),
             nn.Linear(8, 1, bias = False)
         )

Sources/pytorch_src/models/lightning.py
@@ -14,44 +14,19 @@ from pytorch_src.models.distance_model import DistanceLayer
 
 import pytorch_src.default_settings as config
 
-from pytorch_src.data.dataloader import Dataset
-from pytorch_src.data.datareader import PairProcessor
 
 class ImageSiamese(pl.LightningModule):
     def __init__(self, hparams):
         super(ImageSiamese, self).__init__()
         self.hparams = hparams
-        print(hparams)
 
-        pairProcessor = PairProcessor(hparams.clinical_path)
-        train_ids, val_ids, test_ids = pairProcessor.train_test_split(
-            val_ratio = 0.2,
-            test_ratio = hparams.test_ratio,
-            split_model = PairProcessor.TIME_SPLIT,
-            random_seed = 520)
-
-        print(train_ids[0])
-        print(train_ids[-1])
-        print(val_ids[0])
-        print(val_ids[-1])
-        print(test_ids[0])
-        print(test_ids[-1])
-
-        self.train_set = Dataset(train_ids, hparams.clinical_path, hparams.image_path, hparams.radiomics_path, hparams)
-        self.val_set = Dataset(val_ids, hparams.clinical_path, hparams.image_path, hparams.radiomics_path, hparams)
-        self.test_set = Dataset(test_ids, hparams.clinical_path, hparams.image_path, hparams.radiomics_path, hparams)
-
-
-        print(len(self.train_set))
-        print(len(self.val_set))
-        print(len(self.test_set))
         self.use_images = hparams.use_images
         self.use_radiomics = hparams.use_radiomics
 
         self.criterion = nn.BCELoss()
-        self.convolution = ConvolutionLayer()
-        self.fc = FullyConnected()
-        self.distance = DistanceLayer()
+        self.convolution = ConvolutionLayer() if hparams.use_images else nn.Identity()
+        self.fc = FullyConnected(hparams)
+        self.distance = DistanceLayer() if hparams.use_distance else nn.Identity()
         self.log_model_parameters()
 
     def forward(self, iA, iB, rA, rB):
@@ -70,15 +45,15 @@ class ImageSiamese(pl.LightningModule):
             y = self.convolution(iB)
         x = self.fc(x)
         y = self.fc(y)
-        np_x = x.view(-1).detach().cpu().numpy()
-        np_y = y.view(-1).detach().cpu().numpy()
-        # print(np_x)
-        # print(np_y)
-        z = torch.sub(x, y)
+        z = (x - y)
 
         z = self.distance(z)
 
         return torch.sigmoid(z)
+
+    def on_epoch_start(self):
+        print("")
+        print("EPOCH START")
 
     def training_step(self, batch, batch_idx):
         iA = batch['imageA']
@@ -92,12 +67,26 @@ class ImageSiamese(pl.LightningModule):
         output = self.forward(iA, iB, rA, rB)
         loss = self.criterion(output.view(-1), labels.view(-1))
 
-        if(batch_idx % 1000 == 0):
+        if(batch_idx % 1000 == -1):
             np_output = output.view(-1).detach().cpu().numpy()
             print(np_output)
 
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+        np_output = output.view(-1).detach().cpu().numpy()
+        output_class = np.where(np_output < 0.5, 0, 1)
+
+        correct = np.sum(output_class == labels.view(-1).detach().cpu().numpy())
+        total = len(np_output)
+        tensorboard_logs = {'train_loss': loss.item(), 'correct' : correct, 'total': total}
+        return {'loss': loss, 'custom_logs': tensorboard_logs}
+
+    def training_epoch_end(self, outputs):
+        avg_loss = np.mean([x['custom_logs']['train_loss'] for x in outputs])
+        correct = np.sum([x['custom_logs']['correct'] for x in outputs])
+        total =   np.sum([x['custom_logs']['total'  ] for x in outputs])
+        tensorboard_logs = {
+            'avg_loss' : avg_loss,
+            'train_CI' : correct/total}
+        return {'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
         iA = batch['imageA']
@@ -123,7 +112,7 @@ class ImageSiamese(pl.LightningModule):
         correct = np.sum([x['correct'] for x in outputs])
         tensorboard_logs = {'val_loss': avg_loss,
                             'val_CI' : correct/total}
-        return {'val_loss' : avg_loss, 'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
+        return {'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
 
     def test_step(self, batch, batch_idx):
         iA = batch['imageA']
@@ -151,48 +140,40 @@ class ImageSiamese(pl.LightningModule):
                             'test_CI' : correct/total}
         return {'test_loss' : avg_loss, 'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
 
-
     def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=1e-4, momentum=0.9)
+        optimizer = torch.optim.SGD(self.parameters(),
+                                    lr=self.hparams.learning_rate,
+                                    momentum=self.hparams.momentum,
+                                    weight_decay = self.hparams.weight_decay)
 
-    def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_set,
-                                           batch_size = self.hparams.batch_size,
-                                           shuffle = True,
-                                           num_workers=self.hparams.num_workers)
+        scheduler = torch.optim.lr_scheduler .MultiStepLR(
+            optimizer,
+            milestones=self.hparams.sc_milestones,
+            gamma=self.hparams.sc_gamma)
 
-    def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_set,
-                                           batch_size = self.hparams.batch_size,
-                                           shuffle = True,
-                                           num_workers=self.hparams.num_workers)
-
-    def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_set,
-                                           batch_size = self.hparams.batch_size,
-                                           shuffle = True,
-                                           num_workers=self.hparams.num_workers)
-
-
+        return [optimizer], [scheduler]
 
     def add_model_specific_args(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
-        ## DATALOADER
-        parser.add_argument("--num-workers", default=16, type=int)
-        parser.add_argument("--batch-size", default=config.BATCH_SIZE, type=int)
-        parser.add_argument("--test-ratio", default=0.3, type=float)
 
         ## NETWORK
-        # parser.add_argument('--fc-layers', type=int, nargs='+', default=[1671, 480, 240, 128])
-        # parser.add_argument('--d-layers', type=int, nargs='+', default=[128, 48, 16, 1])
+        parser.add_argument('--fc-layers', type=int, nargs='+',
+                            default = config.FC_LAYERS)
+        parser.add_argument('--dropout', type=float, nargs='+',
+                            default = config.DROPOUT)
 
-        parser.add_argument('--dropout', type=float, default = 0.2)
+        ## OPTIMIZER
+        parser.add_argument('--learning-rate', type=float, default=config.LR)
+        parser.add_argument('--momentum', type=float, default=config.MOMENTUM)
+        parser.add_argument('--weight-decay', type=float, default=config.WEIGHT_DECAY)
+        parser.add_argument('--sc-milestones', type=int, nargs='+',
+                            default = config.SC_MILESTONES)
+        parser.add_argument('--sc-gamma', type=float, default=config.SC_GAMMA)
 
-        ## Training
-        parser.add_argument("--epochs", default=10, type=int)
-        # network params
+        parser.add_argument('--use-distance', action='store_true', default=config.USE_DISTANCE)
+        ##TODO: implement Distance layer
+        parser.add_argument('--d-layers', type=int, nargs='+', default=config.D_LAYERS)
         return parser
-
 
     def log_model_parameters(self):
         print("PARAMETERS**********************************************")

Sources/pytorch_src/train_lightning.py
@@ -14,20 +14,74 @@ import torch
 import torch.nn as nn
 
 import pytorch_src.default_settings as config
+from pytorch_src.data.datareader import PairProcessor
+from pytorch_src.data.dataloader import Dataset
 
 from pytorch_lightning import Trainer
 
 ################
 #     MAIN     #
 ################
+def Create_Dataloader(ids, hparams):
+        dataset = Dataset(ids, hparams.clinical_path, hparams.image_path, hparams.radiomics_path, hparams)
+        return torch.utils.data.DataLoader(dataset,
+                                           batch_size = hparams.batch_size,
+                                           shuffle = True,
+                                           num_workers= hparams.num_workers)
+
 def deepCinet(args):
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    np.random.seed(520)
+    torch.manual_seed(520)
+
+    pairProcessor = PairProcessor(args.clinical_path)
     hdict = vars(args)
-    hdict['use_images'] = config.USE_IMAGES
-    hdict['use_radiomics'] = config.USE_RADIOMICS
     hparams = argparse.Namespace(**hdict)
-    siamese_model = ImageSiamese(hparams=hparams)
-    trainer = Trainer(min_epochs = args.epochs, max_epochs=args.epochs, gpus=1, accumulate_grad_batches = 1)
-    trainer.fit(siamese_model)
+    print(hparams)
+
+    if(hparams.use_kfold):
+        folds, test_ids = pairProcessor.k_cross_validation(
+            n_splits = hparams.folds,
+            test_ratio = hparams.test_ratio,
+            random_seed = 520)
+        test_dl = Create_Dataloader(test_ids, hparams)
+        for train_ids, val_ids in folds:
+            train_dl = Create_Dataloader(train_ids, hparams)
+            val_dl = Create_Dataloader(val_ids, hparams)
+
+            siamese_model = ImageSiamese(hparams=hparams)
+            trainer = Trainer(min_epochs = args.epochs,
+                              max_epochs = args.epochs,
+                              gpus=1,
+                              accumulate_grad_batches = 1,
+                              weights_summary ='full',
+                              enable_benchmark = False)
+            trainer.fit(siamese_model,
+                        train_dataloader = train_dl,
+                        val_dataloaders = val_dl,
+                        test_dataloaders = test_dl)
+            print("")
+            print("END FOLD")
+    else:
+        train_ids, val_ids, test_ids = pairProcessor.train_test_split(
+            val_ratio = hparams.val_ratio,
+            test_ratio = hparams.test_ratio,
+            split_model = PairProcessor.TIME_SPLIT,
+            random_seed = 520)
+        train_dl = Create_Dataloader(train_ids, hparams)
+        val_dl = Create_Dataloader(val_ids, hparams)
+        test_dl = Create_Dataloader(test_ids, hparams)
+        siamese_model = ImageSiamese(hparams=hparams)
+        trainer = Trainer(min_epochs = args.epochs,
+                          max_epochs=args.epochs,
+                          gpus=1,
+                          accumulate_grad_batches = 1,
+                          weights_summary='full')
+        trainer.fit(siamese_model,
+                    train_dataloader = train_dl,
+                    val_dataloaders = val_dl,
+                    test_dataloaders = test_dl)
 
 def main(args) -> None:
     """
@@ -46,11 +100,27 @@ if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser()
 
-        # Dataloader
+        ## DATALOADER ######
         data_arg = add_argument_group('Data')
         data_arg.add_argument('--clinical-path', type=str, default=config.CLINICAL_PATH, help='Path to clinical variables')
         data_arg.add_argument('--radiomics-path', type=str, default=config.RADIOMICS_PATH, help='Path to radiomics features')
+        data_arg.add_argument('--use-radiomics', action='store_true', default=config.USE_RADIOMICS)
         data_arg.add_argument('--image-path', type=str, default=config.IMAGE_PATH, help='Path to patient CTs')
+        data_arg.add_argument('--use-images', action='store_true', default=config.USE_IMAGES)
+        data_arg.add_argument("--num-workers", default=config.NUM_WORKERS, type=int)
+        data_arg.add_argument("--batch-size", default=config.BATCH_SIZE, type=int)
+        data_arg.add_argument("--test-ratio", default=config.TEST_RATIO, type=float)
+        data_arg.add_argument("--val-ratio", default=config.VAL_RATIO, type=float)
+
+        data_arg.add_argument("--folds", default=config.FOLDS, type=int)
+        data_arg.add_argument('--use-kfold', action='store_true', default=config.USE_FOLDS)
+        ####################
+
+        ## TRAINING ########
+        parser.add_argument("--epochs", default=config.EPOCHS, type=int)
+        ####################
+
+
 
         parser = ImageSiamese.add_model_specific_args(parser)
         args, unparsed = parser.parse_known_args()